f0a8552d4e2b0e600a546be02be04ce7cf8461b8
Isaac Wang <isaacwang4076@gmail.com>
2016-12-30 15:22:38 -0800

    Basic camera set up

--- UnifyIDChallenge/app/src/main/java/com/example/isaacwang/unifyidchallenge/MainActivity.java
+++ UnifyIDChallenge/app/src/main/java/com/example/isaacwang/unifyidchallenge/MainActivity.java
@@ -1,13 +1,90 @@
 package com.example.isaacwang.unifyidchallenge;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Camera camera;
+    private CameraPreview cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Do we have a camera?
+        if (checkCameraHardware(getApplicationContext())) {
+
+            // Do we still need permission?
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                //ask for authorisation
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
+            }
+        }
+    }
+
+    public void beginScan(View v) {
+        // if camera is set up successfully
+        if (setUpCamera()) {
+
+            // get rid of button
+            ((LinearLayout) findViewById(R.id.mainLayout)).removeView(findViewById(R.id.button));
+
+            // get to work
+            capturePhotos();
+        }
+    }
+
+    // returns whether camera is set up successfully
+    private boolean setUpCamera() {
+        camera = getCameraInstance();
+        if (camera == null) {
+            return false;
+        }
+
+        // initialize the preview
+        cameraPreview = new CameraPreview(this, camera);
+        // add it to the layout
+        ((FrameLayout) findViewById(R.id.camera_preview)).addView(cameraPreview);
+
+        return true;
+    }
+
+    private void capturePhotos() {
+
+    }
+
+    // Does this device have a camera?
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    // Get the camera if we have permission
+    // Returns null if we hit an exception
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
     }
 }
