540e5226867ef0275475e0766ee72af5d06d63d1
Isaac Wang <isaacwang4076@gmail.com>
2016-12-30 21:46:53 -0800

    Slowly, painfully, finally got encryption working.

--- UnifyIDChallenge/app/src/main/java/com/example/isaacwang/unifyidchallenge/MainActivity.java
+++ UnifyIDChallenge/app/src/main/java/com/example/isaacwang/unifyidchallenge/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -20,16 +21,32 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.Timer;
 import java.util.TimerTask;
 import android.graphics.Matrix;
 import android.widget.ImageView;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Camera camera;
     private CameraPreview cameraPreview;
+    private Cipher cipher;
     private int numberOfPhotos = 0;
     private int numberShown = 0;
     private byte[][] allData = new byte[10][];
@@ -38,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int PAUSE_TIME  = 500;
     private static final String TAG = "matag";
     private static final String FILENAME = "super_secure_file_do_not_open";
-    private byte[] key;
+    private AESCrypt myCrypt;
 
     // used in getOutputMediaFile
     public static final int MEDIA_TYPE_IMAGE = 1;
@@ -85,6 +102,21 @@ public class MainActivity extends AppCompatActivity {
                 ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 50);
             }
         }
+        /*try {
+            byte[] key = ("ABCDEFGHIJKLMNOP").getBytes();
+            secretKeySpec = new SecretKeySpec(key, "AES");
+
+            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        } catch (NoSuchAlgorithmException e) {
+
+        } catch (NoSuchPaddingException e) {
+
+        }*/
+        try {
+            myCrypt = new AESCrypt("password");
+        } catch (java.lang.Exception e) {}
+
     }
 
     public void beginScan(View v) {
@@ -201,13 +233,27 @@ public class MainActivity extends AppCompatActivity {
         // delete old data
         deleteOldData();
 
+        // init cipher
+        /*try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            Log.d(TAG, "Cipher encrypt init successful ");
+        } catch (InvalidKeyException e) {
+            Log.d(TAG, e.getMessage());
+        }*/
+
         // write current data
         for (int i = 0; i < TOTAL_PHOTOS; i ++) {
             try {
                 FileOutputStream fos = openFileOutput(FILENAME + i, Context.MODE_PRIVATE);
-                fos.write(bytes[i]);
+                /*CipherOutputStream cos = new CipherOutputStream(fos, cipher);
+                cos.write(bytes[i]);
+                cos.flush();
+                Log.d(TAG, "finished writing data to file " + i + ", bytes written: " + fos.getChannel().size());
+                cos.close();*/
+                try {
+                    fos.write(myCrypt.encrypt(bytes[i]));
+                } catch (java.lang.Exception e) {}
                 fos.close();
-                Log.d(TAG, "finished writing data to file " + i);
             } catch (FileNotFoundException e) {
                 Log.d(TAG, "File not found: " + e.getMessage());
             } catch (IOException e) {
@@ -229,15 +275,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private byte[][] readDataFromFile() {
-        byte[][] readBytes = new byte[10][];
 
+        // array of byte arrays to read into
+        byte[][] readBytes = new byte[10][];
+        /*
+        // init cipher
+        try {
+            byte[] IV = ("ABCDEFGHIJKLMNOP").getBytes();
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec (IV));
+            Log.d(TAG, "Cipher decrypt init successful ");
+        } catch (InvalidKeyException e) {
+            Log.d(TAG, e.getMessage());
+        } catch (InvalidAlgorithmParameterException e) {
+            Log.d(TAG, e.getMessage());
+        }*/
+
+        // read data
         for (int i = 0; i < TOTAL_PHOTOS; i++) {
             try {
                 FileInputStream fis = openFileInput(FILENAME + i);
+                /*CipherInputStream cis = new CipherInputStream(fis, cipher);
+                //readBytes[i] = new byte[(int) fis.getChannel().size()];
+                readBytes[i] = new byte[allData[i].length];
+
+                cis.read(readBytes[i]);
+                cis.close();*/
                 readBytes[i] = new byte[(int) fis.getChannel().size()];
                 fis.read(readBytes[i]);
                 fis.close();
-                Log.d(TAG, "finished reading data from file " + i);
+                try {
+                    readBytes[i] = myCrypt.decrypt(readBytes[i]);
+                } catch (java.lang.Exception e) {}
+
+                Log.d(TAG, "finished reading data from file " + i + ", bytes read: " + readBytes[i].length);
+
             } catch (FileNotFoundException e) {
                 Log.d(TAG, "File not found: " + e.getMessage());
             } catch (IOException e) {
@@ -281,4 +352,56 @@ public class MainActivity extends AppCompatActivity {
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
+
+    private class AESCrypt {
+
+        private final Cipher cipher;
+        private final SecretKeySpec key;
+        private AlgorithmParameterSpec spec;
+
+
+        public AESCrypt(String password) throws Exception
+        {
+            // hash password with SHA-256 and crop the output to 128-bit for key
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            digest.update(password.getBytes("UTF-8"));
+            byte[] keyBytes = new byte[32];
+            System.arraycopy(digest.digest(), 0, keyBytes, 0, keyBytes.length);
+
+            cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+            key = new SecretKeySpec(keyBytes, "AES");
+            spec = getIV();
+        }
+
+        public AlgorithmParameterSpec getIV()
+        {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+            IvParameterSpec ivParameterSpec;
+            ivParameterSpec = new IvParameterSpec(iv);
+
+            return ivParameterSpec;
+        }
+
+        public byte[] encrypt(byte[] og) throws Exception
+        {
+            //String plainText = new String(og);
+            cipher.init(Cipher.ENCRYPT_MODE, key, spec);
+            byte[] encrypted = cipher.doFinal(og);
+            //String encryptedText = new String(Base64.encode(encrypted, Base64.DEFAULT), "UTF-8");
+
+            return encrypted;
+        }
+
+        public byte[] decrypt(byte[] notOG) throws Exception
+        {
+            //String cryptedText = new String(notOG);
+            cipher.init(Cipher.DECRYPT_MODE, key, spec);
+            //byte[] bytes = Base64.decode(cryptedText, Base64.DEFAULT);
+            byte[] decrypted = cipher.doFinal(notOG);
+            //String decryptedText = new String(decrypted, "UTF-8");
+
+            return decrypted;
+        }
+    }
+
 }
